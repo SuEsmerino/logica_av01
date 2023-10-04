d723b99c870d09b7b372999cac3cd563133dd45d
SuEsmerino <126846253+SuEsmerino@users.noreply.github.com>
2023-10-03 23:11:58 -0300

    av
    av01_Prova Algoritmos e Variáveis modulo 1

---
+++ av01.py
@@ -0,0 +1,22 @@
+# Crie um exemplo de cada uma das principais tipagens de variáveis.
+
+nome = str(input("Digite seu nome:"))
+
+idade = int(input("Digite sua idade:"))
+
+altura = float(input("Digite sua altura: "))
+
+tipoBoolenao = bool(input(
+    1>2
+    
+))
+
+# Crie uma solicitação de dados ao usuário.
+
+informacoes = str(input("Digite seu nome:"))
+informacoes02 = int(input("Digite sua idade:"))
+
+
+# Imprima a solicitação anterior na tela com uma mensagem personalizada.
+
+print(f"Seu nome é {informacoes} e você tem {informacoes02} anos, seja bem vindo a Infinity.")
